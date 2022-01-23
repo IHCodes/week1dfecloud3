79c8a841c933af3aaa9b18209a5aa7d6f7d75344
Imam <imam@sky.com>
2022-01-23 14:33:08 +0000

    IH 23/01/22 Python

---
+++ PythonTest1.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+
+if age >= 105:
+    print("still breathing? considered of booking a funeral service?")
+elif age >= 100:
+    print("not still dead? wow you made it to 100!!! good for you")
+elif age >= 85:
+    print("You are above 85")
+elif age >= 50:
+    print("You are between 50 and 85")
+elif age >= 20:
+    print("You are between 20 and 50")
+else:
+    print("You are below 20 years old")
