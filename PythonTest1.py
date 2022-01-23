0db61456a57bb3db91ae359199e2b0dde3128380
imamhussain1986 <97616881+imamhussain1986@users.noreply.github.com>
2022-01-23 14:38:38 +0000

    IH 23/01/22 Python

--- PythonTest1.py
+++ PythonTest1.py
@@ -1,6 +1,8 @@
 age = int(input("Enter your age: "))
 
-if age >= 105:
+if age>= 110:
+    print("your truly special")
+elif age >= 105:
     print("still breathing? considered of booking a funeral service?")
 elif age >= 100:
     print("not still dead? wow you made it to 100!!! good for you")
