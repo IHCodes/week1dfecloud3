9cf31b1d530edaa1284c7c4365d26b1032906b76
Imam <imam@sky.com>
2022-01-23 15:12:35 +0000

    IH 23/01/22 PythonAmendingTest

--- testing8.py
+++ testing8.py
@@ -1,4 +1,4 @@
-list1 = ["Tim","tim", "harry", "Harry", "dave", "Dave"]
+list1 = ["Tim","tim", "harry", "Harry", "dave", "Dave", "mike", "Mike"]
 var1 = input("Please enter your name: ")
 
 if var1 in list1:
